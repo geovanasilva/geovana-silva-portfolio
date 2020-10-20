02b6526aa45780a4e41b3c76d96f75c1376dff83
Geovana Silva <sts.geovana@gmail.com>
2020-10-20 15:44:50 -0300

    feat: reset css, new imgs and containers

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/App.js",
+  entry: "./src/app.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "dist/bundle.js",
